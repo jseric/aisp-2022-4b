3692593123d1c6f96fd52b780be33a007c729e4a
Damjana Baric <dbaric01@fesb.hr>
2022-11-03 00:26:18 +0100

    Added menu option and finished implementation of removeNode and findLName functions

--- linkedListsTask.c
+++ linkedListsTask.c
@@ -6,9 +6,11 @@
 
 int main()
 {
-    Node* head = NULL;
-    head = (Node*)malloc(sizeof(Node));
-    if(NULL == head){
+    int choice = 0;
+    Node *head = NULL;
+    head = (Node *)malloc(sizeof(Node));
+    if (NULL == head)
+    {
         printf("Initialisation failed!\n");
         return -1;
     }
@@ -18,8 +20,66 @@ int main()
     insertEnd(head, "Ime", "Prezime", 1998);
     insertStart(&head, "Ivo", "Anic", 1999);
     insertEnd(head, "Jure", "Horvat", 2000);
-    
-    printList(head->next);
+
+    do
+    {
+        printf("<<<<<< MENU >>>>>>\n");
+        printf("\t0 - end\n");
+        printf("\t1 - add person to the beginning of list\n");
+        printf("\t2 - add person to the end of list\n");
+        printf("\t3 - find person by last name\n");
+        printf("\t4 - delete from list\n");
+        printf("\t5 - print the list\n");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+        {
+            char name[32];
+            char lastName[32];
+            int yearOfBirth;
+            printf("Name Lastname Birth year:\n");
+            scanf("%s %s %d", name, lastName, yearOfBirth);
+            insertStart(&head, name, lastName, yearOfBirth);
+            break;
+        }
+        case 2:
+        {
+            char name[32];
+            char lastName[32];
+            int yearOfBirth;
+            printf("Name Lastname Birth year:\n");
+            scanf("%s %s %d", name, lastName, yearOfBirth);
+            insertEnd(head, name, lastName, yearOfBirth);
+            break;
+        }
+        case 3:
+        {
+            char lastName[32];
+            printf("Lastname: ");
+            scanf("%s", lastName);
+            findLName(&head, lastName);
+            break;
+        }
+        case 4:
+        {
+            char lastName[32];
+            printf("Lastname: ");
+            scanf("%s", lastName);
+            removeNode(&head, lastName);
+            break;
+        }
+        case 5:
+            printList(head->next);
+            break;
+        default:
+            break;
+        }
+    } while (choice != 0);
 
     return 0;
 }
--- listMethods.h
+++ listMethods.h
@@ -5,9 +5,10 @@
 
 int insertStart(Node **head, char nameIn[], char lastnameIn[], int yobIn)
 {
-    Node* newNode;
-    newNode = (Node*)malloc(sizeof(Node));
-    if(NULL == newNode){
+    Node *newNode;
+    newNode = (Node *)malloc(sizeof(Node));
+    if (NULL == newNode)
+    {
         printf("Memory allocation failed!\n");
         return -1;
     }
@@ -21,21 +22,22 @@ int insertStart(Node **head, char nameIn[], char lastnameIn[], int yobIn)
 
     return 1;
 }
-int insertEnd (Node *head, char nameIn[], char lastnameIn[], int yobIn)
+int insertEnd(Node *head, char nameIn[], char lastnameIn[], int yobIn)
 {
-    Node* position = head;
-    
-    while(position->next != NULL){
+    Node *position = head;
+
+    while (position->next != NULL)
+    {
         position = position->next;
     }
 
-    position->next = (Node*)malloc(sizeof(Node));
-    if(NULL == position->next){
+    position->next = (Node *)malloc(sizeof(Node));
+    if (NULL == position->next)
+    {
         printf("Memory allocation failed!\n");
         return -1;
     }
-    
-    
+
     strcpy(position->next->data.name, nameIn);
     strcpy(position->next->data.lastname, lastnameIn);
     position->next->data.yearOfBirth = yobIn;
@@ -45,26 +47,50 @@ int insertEnd (Node *head, char nameIn[], char lastnameIn[], int yobIn)
     return 1;
 }
 
-int printList(Node *position){
-    if(NULL == position){
+int printList(Node *position)
+{
+    if (NULL == position)
+    {
         printf("List empty!\n");
         return 0;
     }
-    while (position != NULL){
+    while (position != NULL)
+    {
         printf("First name: %s; Last name: %s; Year of birth: %d\n",
-            position->data.name, position->data.lastname, position->data.yearOfBirth);
+               position->data.name, position->data.lastname, position->data.yearOfBirth);
         position = position->next;
     }
 
     return 1;
 }
 
-int findLName(){
-
-    return 1;
+int findLName(Node *head, char targetLName[])
+{
+    if (head == NULL)
+    {
+        return -1;
+    }
+    if (strcmp(head->next->data.lastname, targetLName))
+    {
+        printf("Person found!\nFirst name: %s; Last name: %s; Year of birth: %d\n", head->data.name, head->data.lastname, head->data.yearOfBirth);
+        return 1;
+    }
+    return findLName(head->next, targetLName);
 }
 
-int removeNode(){
-
+int removeNode(Node *head, char targetLName[])
+{
+    Node *temp;
+    while (head->next != NULL)
+    {
+        if (strcmp(head->next->data.lastname, targetLName))
+        {
+            printf("Deletion successful!\n");
+            temp = head->next;
+            head->next = temp->next;
+            free(temp);
+        }
+        head = head->next;
+    }
     return 1;
 }
