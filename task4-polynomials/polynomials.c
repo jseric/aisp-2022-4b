7f1a7f75c97a36d988f93ed5be5ea92cea581c49
Damjana Baric <dbaric01@fesb.hr>
2022-11-17 00:04:09 +0100

    Defining all the necessarily functions

--- task4-polynomials/polynomials.c
+++ task4-polynomials/polynomials.c
@@ -1,5 +1,7 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct _Polynomial
 {
@@ -7,148 +9,215 @@ typedef struct _Polynomial
     int exponent;
 } Polynomial;
 
-typedef struct _Node
+struct Node
 {
     Polynomial polynomial;
-    Polynomial *next;
-} Node;
+    struct Node *next;
+};
 
-typedef enum
-{
-    false,
-    true
-} boolean;
-
-// readPolynomial from file
-// printPolynomial
-// sumPolynomial
-// addPolynomial
-
-int readPolynomialFromFile(Node *head)
+int readPolynomialFromFile(struct Node *head)
 {
     char fileName[32];
-    FILE *fPolynomialFile;
-    Node *pol;
-    Node *index;
+    FILE *fPolynomialStream;
+    struct Node *pol;
 
-    printf("File to read from: ");
+    printf("\r\nInput file name: ");
     scanf("%s", fileName);
 
-    fPolynomialFile = fopen(fileName, "r");
+    fPolynomialStream = fopen(fileName, "r");
 
-    if (fPolynomialFile == NULL)
+    if (fPolynomialStream == NULL)
     {
         printf("File empty!\n");
         return -1;
     }
-    else
+
+    while (!feof(fPolynomialStream))
     {
-        while (!feof(fPolynomialFile))
+        pol = (struct Node *)malloc(sizeof(struct Node));
+        if (pol == NULL)
         {
-            pol = (Polynomial *)malloc(sizeof(Node));
-            scanf_s(fPolynomialFile, "%d %d", &pol->polynomial.coefficient, &pol->polynomial.exponent);
-
-            if (pol->polynomial.coefficient == 0)
-            {
-                free(pol);
-            }
-            else
-            {
-                index = head->next;
-
-                while (index->next != NULL && index->next->exponent > pol->polynomial.exponent)
-                    index = index->next;
-
-                if (index->next != NULL && index->next->exponent == pol->polynomial.exponent)
-                {
-                    index->next->exponent += pol->polynomial.coefficient;
-                    free(pol);
-                    if (index->next->coefficient == 0)
-                    {
-                        pol = index->next;
-                        index->next = pol->next;
-                        free(pol);
-                    }
-                }
-                else
-                {
-                    pol->next = index->next;
-                    index->next = pol;
-                }
-            }
+            printf("Memory allocation failed!\n");
+            return -1;
         }
-        fclose(fPolynomialFile);
+        fscanf(fPolynomialStream, "%d %d", &pol->polynomial.coefficient, &pol->polynomial.exponent);
+        pol->next = head->next;
+        head->next = pol;
     }
 
     return 0;
 }
 
-int printPolynomial(Node *head)
+int sortPolynomialsByExponent(struct Node *head)
 {
-    boolean isFirstEl = true;
+    struct Node *j, *j_predecessor, *temp, *end;
 
-    while (head != NULL)
+    while (head->next != end)
     {
-        if (isFirstEl == true)
-        {
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent == 0)
-                printf("1");
-            if (head->polynomial.coefficient == -1 && head->polynomial.exponent == 0)
-                printf("-1");
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent > 0)
-                printf("x^%d", head->polynomial.exponent);
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent < 0)
-                printf("x^%d", head->polynomial.exponent);
-            if (head->polynomial.coefficient > 0 && head->polynomial.coefficient != 1 && head->polynomial.exponent > 0)
-                printf("%d x^%d", head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient < 0 && head->polynomial.coefficient != -1 && head->polynomial.exponent > 0)
-                printf("-%d x%d", (-1) * head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient > 0 && head->polynomial.coefficient != 1 && head->polynomial.exponent < 0)
-                printf("%d x^%d", head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient < 0 && head->polynomial.coefficient != -1 && head->polynomial.exponent < 0)
-                printf("-%d x^%d", (-1) * head->polynomial.coefficient, head->polynomial.exponent);
+        j_predecessor = head;
+        j = head->next;
 
-            isFirstEl = false;
+        while (j->next != end)
+        {
+            if (j->polynomial.exponent < j->next->polynomial.exponent)
+            {
+                temp = j->next;
+                j_predecessor->next = temp;
+                j->next = temp->next;
+                temp->next = j;
+                j = temp;
+            }
+            j_predecessor = j;
+            j = j->next;
+        }
+        end = j;
+    }
+
+    return 0;
+}
+
+int sumPolynomials(struct Node *headPol1, struct Node *headPol2, struct Node *headRes)
+{
+    struct Node *pol, *temp;
+
+    while (headPol1 != NULL && headPol2 != NULL)
+    {
+        pol = (struct Node *)malloc(sizeof(struct Node));
+
+        if (pol == NULL)
+        {
+            printf("Failed memory allocation!\n");
+            return -1;
+        }
+
+        if (headPol1->polynomial.exponent > headPol2->polynomial.exponent)
+        {
+            pol->polynomial.coefficient = headPol1->polynomial.coefficient;
+            pol->polynomial.exponent = headPol1->polynomial.exponent;
+            headPol1 = headPol1->next;
+        }
+        else if (headPol1->polynomial.exponent < headPol2->polynomial.exponent)
+        {
+            pol->polynomial.coefficient = headPol2->polynomial.coefficient;
+            pol->polynomial.exponent = headPol2->polynomial.exponent;
+            headPol2 = headPol2->next;
         }
         else
         {
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent == 0)
-                printf(" + 1");
-            if (head->polynomial.coefficient == -1 && head->polynomial.exponent == 0)
-                printf(" - 1");
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent > 0)
-                printf(" + x^%d", head->polynomial.exponent);
-            if (head->polynomial.coefficient == 1 && head->polynomial.exponent < 0)
-                printf(" + x^%d", head->polynomial.exponent);
-            if (head->polynomial.coefficient > 0 && head->polynomial.exponent > 0)
-                printf(" + %d x^%d", head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient < 0 && head->polynomial.exponent > 0)
-                printf(" - %d x^%d", (-1) * head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient > 0 && head->polynomial.exponent < 0)
-                printf(" + %d x^%d", head->polynomial.coefficient, head->polynomial.exponent);
-            if (head->polynomial.coefficient < 0 && head->polynomial.exponent < 0)
-                printf(" - %d x^%d", (-1) * head->polynomial.coefficient, head->polynomial.exponent);
+            pol->polynomial.exponent = headPol1->polynomial.exponent;
+            pol->polynomial.coefficient = headPol1->polynomial.coefficient + headPol2->polynomial.coefficient;
+            headPol1 = headPol1->next;
+            headPol2 = headPol2->next;
         }
-
-        head = head->next;
+        pol->next = headRes->next;
+        headRes->next = pol;
+        headRes = headRes->next;
     }
 
-    printf("\r\n");
+    if (headPol1 == NULL)
+        temp = headPol2;
+    else
+        temp = headPol1;
+
+    while (temp != NULL)
+    {
+        pol = (struct Node *)malloc(sizeof(struct Node));
+
+        if (pol == NULL)
+        {
+            printf("Failed memory allocation!\n");
+            return -1;
+        }
+
+        pol->polynomial.coefficient = temp->polynomial.coefficient;
+        pol->polynomial.exponent = temp->polynomial.exponent;
+
+        temp = temp->next;
+        pol->next = headRes->next;
+        headRes = headRes->next;
+    }
+
+    return 0;
+}
+
+int multiplyPolynomials(struct Node *headPol1, struct Node *headPol2, struct Node *headRes)
+{
+    struct Node *i, *j, *pol, *temp;
+    i = headPol1;
+
+    while (i != NULL)
+    {
+        j = headPol2;
+        while (j != NULL)
+        {
+            pol = (struct Node *)malloc(sizeof(struct Node));
+            if (pol == NULL)
+            {
+                printf("Failed memory allocation!\n");
+                return -1;
+            }
+
+            pol->polynomial.exponent = i->polynomial.exponent + j->polynomial.exponent;
+            pol->polynomial.coefficient = i->polynomial.coefficient * j->polynomial.coefficient;
+            temp = headRes;
+
+            while (temp->next != NULL && temp->next->polynomial.exponent > pol->polynomial.exponent)
+                temp = temp->next;
+
+            if (temp->next != NULL && temp->next->polynomial.exponent == pol->polynomial.exponent)
+            {
+                temp->next->polynomial.coefficient += pol->polynomial.coefficient;
+                free(pol);
+            }
+            else
+            {
+                pol->next = temp->next;
+                temp->next = pol;
+            }
+            j = j->next;
+        }
+        i = i->next;
+    }
+
+    return 0;
+}
+
+int printList(struct Node *head)
+{
+    if (head == NULL)
+        printf("The list is empty!\n");
+
+    while (head != NULL)
+    {
+        printf("%d x^%d", head->polynomial.coefficient, head->polynomial.exponent);
+        head = head->next;
+        if (head != NULL)
+            printf(" + ");
+    }
+
+    return 0;
+}
+
+int deleteList(struct Node *head)
+{
+    struct Node *temp;
+    while (head->next != NULL)
+    {
+        temp = head->next;
+        head->next = temp->next;
+        free(temp);
+    }
+
+    return 0;
 }
 
 int main()
 {
-    Node *head = NULL;
-    head = (Node *)malloc(sizeof(Node));
-    if (NULL == head)
-    {
-        printf("Initialisation failed!\n");
-        return -1;
-    }
-    head->next = NULL;
-
-    readPolynomialFromFile(head);
-    printPolynomial(head);
+    struct Node *headPol1, *headPol2, *headSum, *headMult;
+    headPol1->next = NULL;
+    headPol2->next = NULL;
+    headSum->next = NULL;
+    headMult->next = NULL;
 
     return 0;
 }
